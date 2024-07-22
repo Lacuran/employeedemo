6d93e2b077d16d4a52b06a24c299f23434764a94
Lacuran <kornel.wrzesien@gmail.com>
2024-07-22 14:18:40 +0200

    Import cleanup

--- src/main/java/com/emplyee/employeedemo/controller/GraphQLController.java
+++ src/main/java/com/emplyee/employeedemo/controller/GraphQLController.java
@@ -1,7 +1,6 @@
 package com.emplyee.employeedemo.controller;
 
 import com.emplyee.employeedemo.service.GraphQLService;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import graphql.ExecutionResult;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
